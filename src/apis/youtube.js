2c52b6c41991b65e477be2689a9db413e1bb2527
Amalia <amalia.fergin@gmail.com>
2019-06-25 20:08:55 -0700

    on form submit

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyAshxkX-hi68cgNbP4bS1PLxXoi11eg-uY'
+
+export default axios.create({
+  baseURL: `https://www.googleapis.com/youtube/v3`,
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+}); 
