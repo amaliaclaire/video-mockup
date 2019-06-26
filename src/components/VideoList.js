bed22422710b9cc2b4140f4b350cd513fb4a2728
Amalia <amalia.fergin@gmail.com>
2019-06-26 12:15:03 -0700

    list being rendered to the front end of the app.js

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
+const VideoList = ({videos}) => {
+  const renderedList = videos.map(video => {
+    return <VideoItem />
+  });
 
-const VideoList = ({ videos }) => {
-  // props.videos
-  return <div>{videos.length}</div>
-};
+  return <div>{renderedList}</div>
+}
 
-export default VideoList;
+export default VideoList; 
