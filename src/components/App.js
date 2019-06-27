2733b3c5b76896f79f080dd284a7d2e0d451c1f1
Amalia <amalia.fergin@gmail.com>
2019-06-26 20:38:03 -0700

    onvideoselect function is not working, i think it's undefined need to redo it

--- src/components/App.js
+++ src/components/App.js
@@ -5,23 +5,33 @@ import VideoList from './VideoList';
 import VideoItem from './VideoItem';
 
 class App extends React.Component {
-  state = {videos: []}
+  state = {videos: [], selectedVideo: null}
+
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
-
-
      this.setState({videos: response.data.items })
   };
+
+  onVideoSelect = (video) => {
+    console.log('fromthe App!', video);
+  }
+
+
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit}/>
-        <VideoList videos={this.state.videos}/>
+        <SearchBar
+        onFormSubmit={this.onTermSubmit}
+        />
+        <VideoList
+        onVideoSelect={this.state.onVideoSelect}
+        videos={this.state.videos}
+        />
       </div>
     )
   }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({videos}) => {
+const VideoList = ({videos, onVideoSelect }) => {
   const renderedList = videos.map(video => {
-    window.thefuckingvideo = video;
-    return <VideoItem video={video}/>
+    return <VideoItem onVideoSelect={onVideoSelect} video={video}/>
   });
 
-  return <div>{renderedList}</div>
+  return <div className="ui relaxed divided list">{renderedList}</div>
 }
 
 export default VideoList;
