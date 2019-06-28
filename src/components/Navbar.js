e70d05dc36a4933126f26d4be279ff9bdf9863a2
Amalia <amalia.fergin@gmail.com>
2019-06-28 14:26:16 -0700

    color change in navbar to black

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,29 +5,33 @@ class Navbar extends React.Component {
 
   render() {
     return (
-      <div className="ui secondary menu">
-        <button className="active item">
-          Home
-        </button>
-        <button className="item">
-           Messages
-        </button>
-        <button className="item">
-           Friends
-        </button>
 
-        <div className="right menu">
-          <div className="item">
-            <div class="ui icon input">
-              <input type="text" placeholder="search"/>
-              <i class="search link icon"></i>
+      <div className="ui inverted segment">
+        <div className="ui inverted secondary menu">
+          <button className="active orange item">
+            Home
+          </button>
+          <button className="item">
+             Messages
+          </button>
+          <button className="item">
+             Friends
+          </button>
+
+          <div className="right menu">
+            <div className="item">
+              <div class="ui icon input">
+                <input type="text" placeholder="search"/>
+                <i class="search link icon"></i>
+              </div>
+              <button className="ui item">
+              Logout
+              </button>
             </div>
-            <button className="ui item">
-            Logout
-            </button>
           </div>
         </div>
       </div>
+
     )
   }
 }
