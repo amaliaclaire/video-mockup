0322e16ae7f51662fde3887576e8044e19d38dec
Amalia <amalia.fergin@gmail.com>
2019-06-28 13:16:00 -0700

    add

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,20 @@ import VideoDetail from './VideoDetail'
 class App extends React.Component {
   state = {videos: [], selectedVideo: null}
 
+  componentDidMount() {
+    this.onTermSubmit(`cam girls`)
+  }
+
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
-     this.setState({videos: response.data.items })
+     this.setState({
+       videos: response.data.items,
+       selectedVideo: response.data.items[0]
+     });
   };
 
   onVideoSelect = (video) => {
